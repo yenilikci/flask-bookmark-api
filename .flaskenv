de7665241ee39ee597c9ffcb53b4fdf9fde815ca
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-03 16:15:05 +0300

    database and models setup

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 export FLASK_ENV=development
 export FLASK_APP=src
+export SQLALCHEMY_DATABASE_URI=sqlite:///bookmarks.db
--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import os
 from src.auth import auth
 from src.bookmarks import bookmarks
+from src.database import db
 
 
 def create_app(test_config=None):
@@ -9,7 +10,8 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY=os.environ.get("SECRET_KEY")
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
         )
     else:
         app.config.from_mapping(test_config)
@@ -22,6 +24,8 @@ def create_app(test_config=None):
     def sayHello():
         return jsonify({'message': 'hello world'})
 
+    db.app = app
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
