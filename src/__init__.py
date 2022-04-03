45e6063b3b89fb59cdf26aa2cf9d28874b1c44fa
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-03 15:31:57 +0300

    folder structure, application factory

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import os
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY")
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.get("/")
+    def index():
+        return 'hello world'
+
+    @app.get("/hello")
+    def sayHello():
+        return jsonify({'message': 'hello world'})
+
+    return app
